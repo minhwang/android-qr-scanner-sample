e0d3df253a42d78a9c17e02eb0bc9344d1ba4932
Min Hwang <min.hwang@linecorp.com>
2017-01-18 20:41:41 +0900

    Add logic to check permissions to use a camera

--- app/src/main/java/io/github/minhwang/qrscannersample/MainActivity.java
+++ app/src/main/java/io/github/minhwang/qrscannersample/MainActivity.java
@@ -1,13 +1,57 @@
 package io.github.minhwang.qrscannersample;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private static final int PERMISSION_REQUEST_CODE = 0;
+    private static final String[] permissionsWeNeed = { Manifest.permission.CAMERA };
+    private ArrayList<String> permissionsDenied = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (shouldRequestPermissions()) {
+            requestPermissionsWeNeed();
+        }
+    }
+
+    private boolean shouldRequestPermissions() {
+        if (this.permissionsDenied == null) { this.checkPermissions(); }
+        return this.permissionsDenied.size() > 0 ? true : false;
+    }
+
+    private void requestPermissionsWeNeed() {
+        String[] permissions = this.permissionsDenied.toArray(new String[this.permissionsDenied.size()]);
+        ActivityCompat.requestPermissions(this, permissions, PERMISSION_REQUEST_CODE);
+    }
+
+    private void checkPermissions() {
+        if (this.permissionsDenied == null) { this.permissionsDenied = new ArrayList<>(); }
+        for (String permission : this.permissionsWeNeed) {
+            if (ActivityCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_DENIED) {
+                this.permissionsDenied.add(permission);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            for (int idx = 0; idx < permissions.length; idx++) {
+                if (grantResults[idx] == PackageManager.PERMISSION_GRANTED) {
+                    this.permissionsDenied.remove(permissions[idx]);
+                }
+            }
+        }
     }
 }
